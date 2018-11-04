ab385d5848ced466abb1d2eec1f0341d4c1d8ebe
Sean Lucius <seanlucius22@icloud.com>
2018-11-04 16:25:45 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
-  if board[index] == "X" || "O"
-    return true
-  elsif board[index] == "" || " " || nil 
+  if board[index] == "" || " " || nil 
     return false
+  elsif board[index] == "X" || "O"
+    return true
   end 
 end 
